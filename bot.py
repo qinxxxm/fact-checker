8fc915b0c25a0c9ae28be15e59cab6f1782981d0
qinxxxm <soldiero3of@gmail.com>
2024-04-12 22:11:17 +0800

    added accept photo functionality

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ active_polls = {}
 
 # Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Hello! Thanks for chatting with me. I am a fact checker. Send a message to fact check.')
+    await update.message.reply_text('Hello! Thanks for chatting with me. I am a fact checker. Send a message or photo to fact check.')
 
 async def process_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_message = update.message.text
@@ -42,13 +42,47 @@ async def process_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
      # Store the poll message ID along with the user ID
     active_polls[poll_message.poll.id] = {
         'user_id': update.message.from_user.id,
-        'original_message': user_message,
+        'message_id': update.message.message_id,  # Store original message_id to be able to reply to later
         'expiration_time': datetime.datetime.now() + datetime.timedelta(seconds=25),  # 10 second buffer
         'true_count': 0,
         'false_count': 0
     }
 
     print("poll id is", poll_message.poll.id)
+
+async def process_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Get the photo file
+    photo_file = await update.message.photo[-1].get_file()
+
+    # Send the photo to the fact-checker group with a poll
+    caption = f"Received an image from a user. Please vote if the content in this image is True or False."
+    await context.bot.send_photo(
+        chat_id=FACT_CHECKER_GROUP_ID,
+        photo=photo_file.file_id,
+        caption=caption
+    )
+    poll_message = await context.bot.sendPoll(
+        chat_id=FACT_CHECKER_GROUP_ID,
+        question="Is the content in the above image True or False?",
+        options=['True', 'False'],
+        is_anonymous=False,
+        allows_multiple_answers=False,
+        open_period=15  # 15 seconds
+    )
+
+    # Notify the user that the image is being reviewed
+    await update.message.reply_text(
+        "Thank you for the image. We are now conducting a poll in our fact-checker group. Please wait for the results."
+    )
+
+    # Store the poll information similar to text messages
+    active_polls[poll_message.poll.id] = {
+        'user_id': update.message.from_user.id,
+        'message_id': update.message.message_id,  # Store original message_id to be able to reply to later
+        'expiration_time': datetime.datetime.now() + datetime.timedelta(seconds=25),  # Adjust as necessary
+        'true_count': 0,
+        'false_count': 0
+    }
 
 async def check_expired_polls(bot):
     while True:
@@ -59,7 +93,7 @@ async def check_expired_polls(bot):
         for poll_id in expired_polls:
             poll_data = active_polls.pop(poll_id)
             user_id = poll_data['user_id']
-            original_message = poll_data['original_message']
+            message_id = poll_data['message_id']
             true_count = poll_data['true_count']
             false_count = poll_data['false_count']
             
@@ -68,17 +102,16 @@ async def check_expired_polls(bot):
             if total_votes > 0:
                 percentage_true = (true_count / total_votes) * 100
                 reply_message = (
-                    f'The poll for the message: \n{original_message}\n has ended.\n\n'
-                    f'Here are the results:\n'
+                    f'The poll has ended. Here are the results:\n\n'
                     f'True: {true_count} votes\n'
                     f'False: {false_count} votes\n'
                     f'Truth percentage of message is is {percentage_true:.2f}%'
                 )
             else:
-                reply_message = f"Sorry, no one replied to the poll for the following message:\n\n{original_message}"
+                reply_message = f"Sorry, no one responded to the poll."
             
             # Send the results back to the user
-            await bot.send_message(chat_id=user_id, text=reply_message)
+            await bot.send_message(chat_id=user_id, text=reply_message, reply_to_message_id=message_id)
         await asyncio.sleep(10)
 
 async def handle_poll(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -103,6 +136,7 @@ if __name__ == '__main__':
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(MessageHandler(filters.TEXT & ~ filters.COMMAND , process_text))
+    app.add_handler(MessageHandler(filters.PHOTO & ~filters.COMMAND, process_photo))
     app.add_handler(PollHandler(handle_poll)) # We will use this to send the poll result after each user adds to the poll, as i cant seem to find a method to get the poll results from the poll message id after it expires
      
     loop = asyncio.get_event_loop() # Create a task for the check_expired_polls coroutine and run it in the event loop
